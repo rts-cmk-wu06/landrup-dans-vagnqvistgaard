2ea0739124a880f3d8a736ce734e945842ecf1f7
vagnqvistgaard <U773554@rts-365.dk>
2022-10-03 16:35:28 +0200

    add installs, file structure, empty placeholder files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import { motion } from "framer-motion"; //move to actual usage later (in 'Welcome')
 
 function App() {
 
+
   return (
     <>
-      ...
+
     </>
-  );
+  )
 }
 
 export default App;
